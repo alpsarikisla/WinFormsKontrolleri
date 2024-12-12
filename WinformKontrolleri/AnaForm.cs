7722e54f72369ef347e67ead951ce58b667c18f1
alpsarikisla <50963484+alpsarikisla@users.noreply.github.com>
2024-12-12 09:25:17 +0300

    girisformyapildi

---
+++ WinformKontrolleri/AnaForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WinformKontrolleri.Model;
+
+namespace WinformKontrolleri
+{
+    public partial class AnaForm : Form
+    {
+        public AnaForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AnaForm_Load(object sender, EventArgs e)
+        {
+            GirisForm frm = new GirisForm();
+            frm.ShowDialog();
+
+            toolStripStatusLabel1.Text = "Kullanıcı = " + LoginUser.user.FullName;
+        }
+
+        private void TSMI_kategori_Click(object sender, EventArgs e)
+        {
+            DataGridKontrolu frm = new DataGridKontrolu();
+            frm.MdiParent = this;
+            frm.WindowState = FormWindowState.Maximized;
+            frm.Show();
+        }
+    }
+}
---
+++ WinformKontrolleri/GirisForm.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WinformKontrolleri
+{
+    partial class GirisForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tb_userName = new System.Windows.Forms.TextBox();
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btn_login);
+            this.groupBox1.Controls.Add(this.btn_close);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tb_password);
+            this.groupBox1.Controls.Add(this.tb_userName);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(436, 140);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kullanıcı Bilgileri";
+            // 
+            // tb_userName
+            // 
+            this.tb_userName.Location = new System.Drawing.Point(125, 32);
+            this.tb_userName.Name = "tb_userName";
+            this.tb_userName.Size = new System.Drawing.Size(290, 20);
+            this.tb_userName.TabIndex = 0;
+            // 
+            // tb_password
+            // 
+            this.tb_password.Location = new System.Drawing.Point(126, 64);
+            this.tb_password.Name = "tb_password";
+            this.tb_password.Size = new System.Drawing.Size(290, 20);
+            this.tb_password.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kullanıcı Adı : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Şifre : ";
+            // 
+            // btn_close
+            // 
+            this.btn_close.Location = new System.Drawing.Point(340, 95);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(75, 23);
+            this.btn_close.TabIndex = 4;
+            this.btn_close.Text = "Kapat";
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(216, 95);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(118, 23);
+            this.btn_login.TabIndex = 4;
+            this.btn_login.Text = "Giriş Yap";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // GirisForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 160);
+            this.ControlBox = false;
+            this.Controls.Add(this.groupBox1);
+            this.MaximumSize = new System.Drawing.Size(476, 199);
+            this.MinimumSize = new System.Drawing.Size(476, 199);
+            this.Name = "GirisForm";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Northwind Ticaret Giriş";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.GirisForm_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_password;
+        private System.Windows.Forms.TextBox tb_userName;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.Button btn_close;
+    }
+}
